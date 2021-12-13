973053a230229f3cd1a111c44b75807718819cde
MikiKru <michal_kruczkowski@o2.pl>
2021-12-13 14:12:14 +0100

    ex 2.4

--- d1/exercises/ex_2_4.py
+++ d1/exercises/ex_2_4.py
@@ -3,7 +3,8 @@ import math
 n = 49
 k = 6
 
-combs = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+# combs = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+combs = math.comb(n, k)
 
 print(f"Ilość kombinacji: {combs}")
 print(f"Szanse na trafienie: {1/combs:.10f}")
