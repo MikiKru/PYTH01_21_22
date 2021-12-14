ee482beaaf292c6064a2fbaca8d9a3a57b751793
MikiKru <michal_kruczkowski@o2.pl>
2021-12-14 12:16:24 +0100

    sequences

---
+++ d2/sequences_operations.py
@@ -0,0 +1,9 @@
+names = ["Michał", "Ala", "Ola", "Adam"]
+
+for index, name in enumerate(names):
+    print("index=" + str(index), "imię=" + name)
+
+text = "ABC"
+print(text * 10)
+
+print('Adam' in names)
