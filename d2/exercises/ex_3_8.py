ab176fdce40a473f781270cca3d49f474f07a07f
MikiKru <michal_kruczkowski@o2.pl>
2021-12-14 13:26:18 +0100

    ex 3.8

--- d2/exercises/ex_3_8.py
+++ d2/exercises/ex_3_8.py
@@ -1,11 +1,11 @@
 months = ('styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień')
 
-quarters = []
-for q_index in range(4):
-    current_months = []
-    for m_index, month in enumerate(months):
-        if m_index // 3 == q_index:
-            current_months.append(month)
-    quarters.append(current_months)
+quarters = [[month for m_index, month in enumerate(months) if m_index // 3 == q_index] for q_index in range(4)]
+# for q_index in range(4):
+#     current_months = []
+#     for m_index, month in enumerate(months):
+#         if m_index // 3 == q_index:
+#             current_months.append(month)
+#     quarters.append(current_months)
 
 print(quarters)
