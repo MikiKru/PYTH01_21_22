c90e7480792a484a8cad0b0a7c3b73621aeaaa67
MikiKru <michal_kruczkowski@o2.pl>
2021-12-14 12:52:55 +0100

    ex 3.6

---
+++ d2/exercises/ex_3_6.py
@@ -0,0 +1,11 @@
+import re
+
+text = "Do geese see God"
+# oczyść tekst -> zostają tylko znaki alfanumeryczne i podniesione do wielkich liter
+clean = ''.join(c.upper() for c in text if c.isalnum())
+# print(re.findall('[a-zA-z]+',text.upper()))
+# for c in text:
+#     if c.isalnum():
+#         clean += c.upper()
+half_len = len(clean) // 2
+print(clean[0:half_len], clean[-1: -(half_len+1):-1])
