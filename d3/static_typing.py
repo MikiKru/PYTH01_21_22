f0d573b3024c73d12e8d55976659db5312661aec
MikiKru <michal_kruczkowski@o2.pl>
2021-12-15 13:38:19 +0100

    static typing

---
+++ d3/static_typing.py
@@ -0,0 +1,6 @@
+def addition(x: float, y: float) -> float:
+    return x + y
+
+
+print(addition(1, 1))
+print(addition("1", "1"))       # typowanie statyczne pomaga rozpoznać sytuację wyjątkowe na wczesnym etapie implementacji
