22e0c36cec9ff6d5ea9f7754a46e521f8b527030
MikiKru <michal_kruczkowski@o2.pl>
2021-12-15 09:37:37 +0100

    ex 3.12

--- d3/exercises/ex_3_12.py
+++ d3/exercises/ex_3_12.py
@@ -4,17 +4,25 @@ p3 = {"name": "Andrzej", "last_name": "Spirytus", "address": ("Gdańsk", "Zbójn
 p4 = {'name': 'Anna', 'last_name': 'Nowak', 'address': ('Gdynia', 'Morska', 32), 'gender': False, 'age': 27}
 p5 = {"name": "Piotr", "last_name": "Sosiński", "address": ("Derc", "Bobrowa", 128), "gender": True, "age": 51}
 p6 = {"name": "Jan,", "last_name": "Jankowski", "address": ("Gdańsk", "Towarowa", 38), "gender": True, "age": 30}
-p7 = {"name": "Woyt", "last_name": "Brozyna", "adderss": ("Pcim koło warszawki", None, 1), "gender": True, "age": 75}
-p8 = {"name": "Katarzyna", "last_name": "Kowalska", "address": ("Gdańsk", "Zbożowa", 4), "gender": True, "age": 40}
+p7 = {"name": "Woyt", "last_name": "Brozyna", "address": ("Pcim koło warszawki", None, 1), "gender": True, "age": 75}
+p8 = {"name": "Katarzyna", "last_name": "Kowalska", "address": ("Gdańsk", "Zbożowa", 4), "gender": False, "age": 40}
 
 users = [p1, p2, p3, p4, p5, p6, p7, p8]
 
 # lista mężczyzn z danego miasta
 # SELECT * FROM users WHERE address[1] = ?;
+city = "Gdańsk"
+users_from_city = []
+for user in users:
+    if user['address'][0] == city:
+        users_from_city.append(user)
 
+for user in users_from_city:
+    print(f"|{user['name']:>15}|{user['last_name']:>15}|{user['address'][0]:>15}|{user['address'][1]:>35}|{user['address'][2]:5}|{user['gender']:5}|{user['age']:5}|")
 
 # lista osób z danego przedziału wiekowego
 # SELECT * FROM users WHERE age BETWEEN ? AND ?;
+
 
 # średnia wieku kobiet o imionach rozpoczynających się na A
 # SELECT avg(age) FROM users WHERE gender = 0 AND name like 'A%';
