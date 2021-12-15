8b48e4d6d679ab669dab05e6804f532fab632163
MikiKru <michal_kruczkowski@o2.pl>
2021-12-15 09:10:27 +0100

    ex 3.12

---
+++ d3/exercises/ex_3_12.py
@@ -0,0 +1,8 @@
+p1 = {"name": "Adam", "last_name": "Kowalski", "address": ("Warszawa", "Królewska", 15), "gender": True, "age": 39}
+p2 = {"name": "Benek", "last_name": "Benkowski", "address": ("Jaroty", "Bartąska", 128), "gender": True, "age": 45}
+p3 = {"name": "Andrzej", "last_name": "Spirytus", "address": ("Gdańsk", "Zbójnicka", 2), "gender": True, "age": 35}
+p4 = {'name': 'Anna', 'last_name': 'Nowak', 'address': ('Gdynia', 'Morska', 32), 'gender': False, 'age': 27}
+p5 = {"name": "BPiotr", "last_name": "BSosiński", "address": ("Derc", "Bobrowa", 128), "gender": True, "age": 51}
+p6 = {"name": "Jan,", "last_name": "Jankowski", "address": ("Gdańsk", "Towarowa", 38), "gender": True, "age": 30}
+p7 = {"name": "Woyt", "last_name": "Brozyna", "adderss": ("Pcim koło warszawki", None, 1), "gender": True, "age": 75}
+print(p1)
