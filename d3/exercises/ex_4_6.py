1bd04fa8896f7332d8d720a6178499473b271d10
MikiKru <michal_kruczkowski@o2.pl>
2021-12-15 13:54:09 +0100

    ex 4.6

---
+++ d3/exercises/ex_4_6.py
@@ -0,0 +1,35 @@
+def day_index(date: str, sep: str = "-") -> int:
+    z: int
+    c: int
+    year, month, day = date.split(sep)
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    z = year - 1 if month < 3 else year
+    c = 0 if month < 3 else 2
+    return (int(23 * month / 9) + day + 4 + year + (z // 4) - (z // 100) + (z // 400) - c) % 7
+
+
+def day_name(day_index: int) -> str:
+    day_name_mapper: tuple
+    day_name_mapper = ('niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota')
+    return day_name_mapper[day_index]
+
+def get_day_name() -> None:
+    sep: str
+    date: str
+    while True:
+        sep = input("wybierz separator \"-\" lub \".\"")
+        if sep != "-" and sep != ".":
+            continue
+        date = input("podaj datę zgodnie z wybranym separatorem: ")
+        if date[4] != "-" and date[2] != ".":
+            continue
+        print(f"Dzień dla podanej daty {date} to {day_name(day_index(date,sep))}")
+        break
+
+
+# day_index(1, "-")     - błąd typu w argumencie
+# day_name("XXX")       - błąd typu w argumencie
+# x = get_day_name()    - błąd typu zwracanej wartości
+get_day_name()
--- d3/static_typing.py
+++ d3/static_typing.py
@@ -1,6 +1,17 @@
+# opis funkcji w komentarzu jest dostępny dla programisty - dostępny w źródłach
 def addition(x: float, y: float) -> float:
+    """
+    Funkcja do obliczania sumy dwóch skłądników.
+
+    :param x: skłądnik pierwszy
+    :param y: składnik drugi
+    :return: suma
+    """
     return x + y
 
 
 print(addition(1, 1))
 print(addition("1", "1"))       # typowanie statyczne pomaga rozpoznać sytuację wyjątkowe na wczesnym etapie implementacji
+
+help(addition)
+print(addition.__doc__)
