b87667bcf60b3fd6bff530bd07c14521c7bf8ec7
MikiKru <michal_kruczkowski@o2.pl>
2021-12-15 13:09:58 +0100

    ex 4.4

--- d3/exercises/ex_4_3_ex_4_4.py
+++ d3/exercises/ex_4_3_ex_4_4.py
@@ -6,7 +6,7 @@ p3 = {"name": "Andrzej", "last_name": "Spirytus", "address": ("Gdańsk", "Zbójn
 p4 = {'name': 'Anna', 'last_name': 'Nowak', 'address': ('Gdynia', 'Morska', 32), 'gender': False, 'age': 27}
 p5 = {"name": "Piotr", "last_name": "Sosiński", "address": ("Derc", "Bobrowa", 128), "gender": True, "age": 51}
 p6 = {"name": "Jan,", "last_name": "Jankowski", "address": ("Gdańsk", "Towarowa", 38), "gender": True, "age": 30}
-p7 = {"name": "Woyt", "last_name": "Brozyna", "address": ("Pcim koło warszawki", None, 1), "gender": True, "age": 75}
+p7 = {"name": "Woyt", "last_name": "Brozyna", "address": ("Pcim koło warszawki", "None", 1), "gender": True, "age": 75}
 p8 = {"name": "Aleksandra", "last_name": "Kowalska", "address": ("Gdańsk", "Zbożowa", 4), "gender": False, "age": 40}
 users = [p1, p2, p3, p4, p5, p6, p7, p8]
 
@@ -40,4 +40,6 @@ print(sorted(users, key=sort_by_last_name))
 print(sorted(users, key=sort_by_age_and_last_name, reverse=True))
 print(sorted(filter(is_man, users), key=sort_by_age_and_last_name, reverse=True))
 
+for user in sorted(filter(is_man, users), key=sort_by_age_and_last_name, reverse=True):
+    print(f"|{user['name']:>15}|{user['last_name']:>15}|{user['address'][0]:>50}|{user['address'][1]:>50}|{user['address'][2]:5}|{user['gender']:5}|{user['age']:5}|")
 
