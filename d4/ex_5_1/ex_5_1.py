cae1714096b553ee9b10503df32b5f79682f6941
MikiKru <michal_kruczkowski@o2.pl>
2022-01-12 10:04:05 +0100

    get/set

--- d4/ex_5_1/ex_5_1.py
+++ d4/ex_5_1/ex_5_1.py
@@ -2,16 +2,26 @@ import datetime
 
 class User:
     def __init__(self, name, last_name, sex, year):
-        self.name = name
-        self.last_name = last_name
-        self.sex = sex
-        self.year = year
+        self.__name = name              # pole silnie prywatne -> niedostępne spoza klasy
+        self.__last_name = last_name
+        self.__sex = sex
+        self.__year = year
     def get_age(self):
-        return datetime.date.today().year - self.year
+        return datetime.date.today().year - self.__year
+    def set_year(self, year):
+        if str(type(year)) == "<class 'int'>":
+            print("zaktualizowano wartość")
+            self.__year = year
+        else:
+            print("błędny typ wartości - pole pozostaje bez zmian!")
+
+    def get_year(self):
+        return self.__year
     def __str__(self):                      # napisowa reprezentacja obiektu
-        return f"{self.name} {self.last_name} płeć: {'M' if self.sex else 'K'} wiek: {self.get_age()}"
+        return f"{self.__name} {self.__last_name} płeć: {'M' if self.__sex else 'K'} wiek: {self.get_age()}"
 
 u1 = User("Adam", "Kowalski", True, 1990)
-u1.year += 1
+u1.set_year(u1.get_year() + 1)
+u1.set_year("1410")             # walidacja!!!
 print(u1)
 
