9795b7c623c577c2ea55dd6d499d31a3396b7844
MikiKru <michal_kruczkowski@o2.pl>
2022-01-12 12:12:02 +0100

    ex 5.5

--- d4/ex_5_5/ex_5_5.py
+++ d4/ex_5_5/ex_5_5.py
@@ -28,6 +28,20 @@ class User:
             print("błędny typ wartości - pole pozostaje bez zmian!")
     def get_age(self):
         return datetime.date.today().year - self.__year
+    def __eq__(self, other):
+        print("wywołanie metody eq na znaku ==")
+        return self.__name == other.__name and self.last_name == other.__last_name and self.__class__ == other.__class__
+    def __gt__(self, other):
+        print("wywołanie metody eq na znaku >")
+        if self.last_name > other.__last_name:
+            return True
+        elif self.last_name < other.__last_name:
+            return False
+        else:
+            if self.name > other.__name:
+                return True
+            else:
+                return False
     def __str__(self):                      # napisowa reprezentacja obiektu
         return f"{self.__name} {self.__last_name} płeć: {'M' if self.__sex else 'K'} wiek: {self.get_age()}"
     name = property(__get_name, __set_name)
@@ -35,6 +49,10 @@ class User:
     sex = property(__get_sex, __set_sex)
     year = property(__get_year, __set_year)
 
-u1 = User("Adam", "Kowalski", True, 1990)
+u1 = User("Jan", "Kowalski", True, 1990)
 u2 = User("Adam", "Kowalski", True, 1991)
 u3 = User("Anna", "Nowak", False, 1990)
+
+print(u1 == u2)
+print(u1 > u2)
+print(u2 > u3)
