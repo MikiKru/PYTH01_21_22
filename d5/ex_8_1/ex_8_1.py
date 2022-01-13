b3df9ab3f093f7b3a61fc5d7ff1227ba568a6576
MikiKru <michal_kruczkowski@o2.pl>
2022-01-13 11:21:13 +0100

    ex_8_1

---
+++ d5/ex_8_1/ex_8_1.py
@@ -0,0 +1,36 @@
+import datetime
+
+class User:
+    def __init__(self, name, last_name, sex, year):
+        self.__name = name              # pole silnie prywatne -> niedostępne spoza klasy
+        self.__last_name = last_name
+        self.__sex = sex
+        self.__year = year
+    def __get_name(self):
+        return self.__name
+    def __get_last_name(self):
+        return self.__last_name
+    def __get_sex(self):
+        return self.__sex
+    def __get_year(self):
+        return self.__year
+    def __set_name(self, name):
+        self.__name = name
+    def __set_last_name(self, last_name):
+        self.__last_name = last_name
+    def __set_sex(self, sex):
+        self.__sex = sex
+    def __set_year(self, year):
+        if str(type(year)) == "<class 'int'>":
+            print("zaktualizowano wartość")
+            self.__year = year
+        else:
+            print("błędny typ wartości - pole pozostaje bez zmian!")
+    def get_age(self):
+        return datetime.date.today().year - self.__year
+    def __str__(self):                      # napisowa reprezentacja obiektu
+        return f"{self.__name} {self.__last_name} płeć: {'M' if self.__sex else 'K'} wiek: {self.get_age()}"
+    name = property(__get_name, __set_name)
+    last_name = property(__get_last_name, __set_last_name)
+    sex = property(__get_sex, __set_sex)
+    year = property(__get_year, __set_year)
